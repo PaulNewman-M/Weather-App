72bebddbdb2f6a36432a98b382824f831f3fdc90
PaulNewman-M <[paulnewman.m@gmail.com]>
2017-05-17 02:40:30 -0500

    deleted unwanted displaylocation

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ function lat(callback) {
     navigator.geolocation.getCurrentPosition(function (position) {
         var lat = position.coords.latitude;
         var lon = position.coords.longitude;
-     var city= displayLocation(lat,lon);
-      alert(city);
         callback.call(null, lat, lon);
     }, function (error) {
         console.log("Something went wrong: ", error);
@@ -11,7 +9,7 @@ function lat(callback) {
 }
 
 function getPosition() {
-  //url( 'http://api.apixu.com/v1/current.json?key=b7816073a00e41bb9f0233424171005&q=dallas' ).load();
+
     lat(function (latitude, longitude) {
         var l = latitude + 
     "," + longitude;  
@@ -24,13 +22,10 @@ getPosition();
 
 function fun(p){ 
   //alert("first"+p);
-
 var $target= $("#target");
  var $tweet= $("#tweet");
-  
- var ajaxCall= $.ajax({
-    
-  url:"https://api.apixu.com/v1/current.json?key=b7816073a00e41bb9f0233424171005&q=dallas",
+ var ajaxCall= $.ajax({ 
+  url:"https://api.apixu.com/v1/current.json?key=b7816073a00e41bb9f0233424171005&q="+p,
    // dataType: "JSON",//(remove jsonp else erro: success 200 "readyState":4 status:load
  //type: "GET",
      //crossDomain: true 
@@ -38,20 +33,15 @@ var $target= $("#target");
   
  // data: "format=json&lang=en",
   ajaxCall.done (function( data ) {
-    
     var img="https:";
-         img +=data.current.condition.icon;
-    if(data.current.is_day==0){
+         img +=data.current.condition.icon;   // weather icon
+    if(data.current.is_day==0){     // if night 
      // alert(img);
        if(data.current.cloud>=0&&data.current.cloud<=100){
-   // $('#my').html(img);  
-          //   $("#cloud").append('<img id ="im" src="https://cdn.apixu.com/weather/64x64/night/116.png"< /img>');
-       
-           // $("#cloud").append('<img id ="im" src="'+img+'"/>');
-          $('body').css("background-image", "url(https://res.cloudinary.com/duqwfkttw/image/upload/v1494995603/tumblr_n3mbu7sEdR1tucbz9o7_500_yuz1qi.gif)");
+          $('body').css("background-image", "url(http://res.cloudinary.com/duqwfkttw/image/upload/v1495003736/giphy_4_icaphx.gif)");
      }}
-      else{
     
+      else{  
     if(data.current.cloud>=90&&data.current.cloud<=100){
     $('body').css("background-image", "url(https://res.cloudinary.com/duqwfkttw/image/upload/v1494994418/giphy_1_yoy0eo.gif)");  
     }
@@ -66,6 +56,7 @@ var $target= $("#target");
      else  {
     $('body').css("background-image", "url(https://res.cloudinary.com/duqwfkttw/image/upload/v1494994995/Sunrise-1024x576_abckfp.jpg)"); 
     }}
+    
         $("#my").html(data.location.name +","+data.location.region);
     $("#para").html(data.location.country );
     $(".tem").html( data.current.temp_c);
@@ -90,6 +81,7 @@ var $target= $("#target");
 })
    
   
-}
+}// function close fun
  
+
 
